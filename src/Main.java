6d6f3a772507176f32f6a54a6a8fd9e2dbac100b
rasoolmohammadi <rasoolkhairmohammadi0049@gmail.com>
2026-07-30 20:57:50 +0200

    Change Color to blueDenim and put flag icons for the currencies.

--- src/Main.java
+++ src/Main.java
@@ -113,9 +113,11 @@ public class Main {
         text.setHorizontalAlignment(JLabel.CENTER);
         text.setFont(new Font("Arial", Font.BOLD, 20));
         text.setPreferredSize(feldGroße);
-        Color denimBlue = new Color(30, 80, 100);
+        Color denimBlue = new Color(0, 51, 79);
         frame.getContentPane().setBackground(denimBlue);
         ((JComponent)frame.getContentPane()).setBorder(BorderFactory.createLineBorder(Color.WHITE,1));
+
+
 
         betragField = new JTextField();
         betragField.setPreferredSize(feldGroße);
@@ -190,6 +192,7 @@ public class Main {
                     betragField.setText("");
                     betragField.requestFocusInWindow();
                     ergebnisField.setForeground(Color.RED);
+
                 }
             }
         });
